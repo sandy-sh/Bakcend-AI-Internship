e89c86e7ca9dfbf43bc03341e71fa4366bf48304
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-07-15 06:33:17 +0700

    Stage 4: full CRUD

--- week-2/crud-api/app.js
+++ week-2/crud-api/app.js
@@ -73,7 +73,58 @@ app.post("/tasks", (req, res) => {
     return res.status(201).json(newTask);
 })
 
+app.put("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const { title, done } = req.body;
 
+    const task = tasks.find(task => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            error: `Task ${id} not found`,
+        })
+    }
+
+    if (title === undefined && done === undefined) {
+        return res.status(400).json({
+            error: "Request body cannot be empty"
+        });
+    }
+
+    if (title !== undefined) {
+        if (typeof title !== "string" || title.trim() === "") {
+            return res.status(400).json({
+                error: "Title is required"
+            });
+        }
+    }
+
+    if (done !== undefined && typeof done !== "boolean") {
+        return res.status(400).json({
+            error: "Done must be a boolean"
+        });
+    }
+
+    task.title = title ?? task.title;
+    task.done = done ?? task.done;
+
+    return res.status(200).json(task);
+})
+
+app.delete("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(task => task.id === id);
+
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            error: `Task ${id} not found`,
+        });
+    }
+
+    tasks.splice(taskIndex, 1);
+
+    return res.status(204).send("No Content");
+})
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
