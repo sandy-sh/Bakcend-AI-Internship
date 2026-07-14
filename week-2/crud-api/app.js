955165176cc5f539d0fd7709abcddf84829654b9
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-07-14 23:35:21 +0700

    Stage 2: read endpoints with 404

--- week-2/crud-api/app.js
+++ week-2/crud-api/app.js
@@ -2,6 +2,23 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+let tasks = [
+    {
+        id: 1,
+        title: "Learn Backend",
+        done: true
+    },
+    {
+        id: 2,
+        title: "Build CRUD API",
+        done: false
+    },
+    {
+        id: 3,
+        title: "Deploy API",
+        done: false
+    }
+]
 
 app.get("/", (req, res) => {
     res.json({
@@ -15,6 +32,22 @@ app.get("/health", (req, res) => {
     res.json({
         "status": "ok",
     })
+})
+
+app.get("/tasks", (req, res) => {
+    res.json(tasks);
+})
+
+app.get("/tasks/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const task = tasks.find((task) => task.id === id);
+
+    if (!task) {
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        });
+    }
+    res.json(task);
 })
 
 app.listen(PORT, () => {
