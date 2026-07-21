6f1c52ed78a27f7a25e2306b833c921b3e185319
Sandy Syarif <sandysyarifhartanto@gmail.com>
2026-07-21 17:26:40 +0700

    Stage 5: Swagger UI

--- week-2/crud-api/app.js
+++ week-2/crud-api/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
-
 const app = express();
 const PORT = 3000;
+const swaggerUi = require("swagger-ui-express");
+const openApiDocument = require("./openapi.json");
+
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(openApiDocument)
+);
 
 app.use(express.json());
 
